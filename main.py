7773f7490dd17fc975a0211417aef27d2dc8d302
Craig Moulds <craig.moulds@gmail.com>
2024-09-03 22:46:22 +0100

    main game loop

--- main.py
+++ main.py
@@ -3,6 +3,17 @@ from constants import *
 
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.fill("black")
+
+        pygame.display.flip()
     print("Starting asteroids!")
     print("Screen width: " + str(SCREEN_WIDTH))
     print("Screen height: " + str(SCREEN_HEIGHT))
